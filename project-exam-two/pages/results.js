a896cda8f0f0cac9b8a1889782726ad784337a77
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-09-26 10:07:00 +0200

    localstorage fix

--- project-exam-two/pages/results.js
+++ project-exam-two/pages/results.js
@@ -3,12 +3,16 @@ import Head from '../components/Head';
 import AccommCard from '../components/AccommCard';
 
 export default function Results() {
-  const resultsList = JSON.parse(localStorage.getItem('ResultsList'));
-  const input = JSON.parse(localStorage.getItem('Input'));
+  let resultsList;
+  let input;
+  if(typeof window !== 'undefined'){
+    resultsList = JSON.parse(localStorage.getItem('ResultsList'));
+    input = JSON.parse(localStorage.getItem('Input'));
+  }
   return(
     <Layout pageId='results'>
       <Head title='Results' description="Find your perfect getaway on Norway's west coast"/>
-      {/* <div className='results__container main'>
+      <div className='results__container main'>
         <div className='results__intro page-intro'>
           <h1>Results</h1>
           <p>Showing {resultsList.length} result(s) for &quot;{input}&quot;</p>
@@ -16,7 +20,7 @@ export default function Results() {
         {resultsList.map((item)=>{
           return <AccommCard key={item.id} acc={item} />
         })}
-      </div> */}
+      </div>
     </Layout>
   )
 }
