b865ee79eb855ee9948c942fa8e778f13dc013c7
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-09-26 15:10:13 +0200

    Fixed footer, modal on large screens

--- project-exam-two/components/Layout.jsx
+++ project-exam-two/components/Layout.jsx
@@ -10,39 +10,41 @@ export default function Layout({children, pageId}) {
     setOpen(!open);
   };
   return(
-    <div id={pageId}>
-      <header>
-        <Link href="/">
-          <a id='page-title'>Holidaze</a>
-        </Link>
-        <nav className={open ? ('show') : ('')}>
-          <Link href="/accommodation">
-            <a className={router.pathname === "/accommodation" ? "nav__link nav__link-active" : "nav__link"}>Accommodation</a>
+    <div id={pageId} className="page">
+      <div className='not-footer'>
+        <header>
+          <Link href="/">
+            <a id='page-title'>Holidaze</a>
           </Link>
-          <Link href="/contact">
-            <a className={router.pathname === "/contact" ? "nav__link nav__link-active" : "nav__link"}>Contact</a>
-          </Link>
-          <Link href="/login">
-            <a className={router.pathname === "/login" ? "nav__link nav__link-active" : "nav__link"}>Administration</a>
-          </Link>
-          <Link href="/dashboard">
-            <a className='hide'></a>
-          </Link>
-          <Link href="/results">
-            <a className='hide'></a>
-          </Link>
-          <Link href="/specific/:param">
-            <a className='hide'></a>
-          </Link>
-        </nav>
-        <div onClick={hamburger} id="hamburger" className={open ? ('open') : ('')}>
-          <div className="burger__layer burger__top"></div>
-          <div className="burger__layer burger__middle1"></div>
-          <div className="burger__layer burger__middle2"></div>
-          <div className="burger__layer burger__bottom"></div>
-        </div>
-      </header>
-      <div className='page-container'>{children}</div>
+          <nav className={open ? ('show') : ('')}>
+            <Link href="/accommodation">
+              <a className={router.pathname === "/accommodation" ? "nav__link nav__link-active" : "nav__link"}>Accommodation</a>
+            </Link>
+            <Link href="/contact">
+              <a className={router.pathname === "/contact" ? "nav__link nav__link-active" : "nav__link"}>Contact</a>
+            </Link>
+            <Link href="/login">
+              <a className={router.pathname === "/login" ? "nav__link nav__link-active" : "nav__link"}>Administration</a>
+            </Link>
+            <Link href="/dashboard">
+              <a className='hide'></a>
+            </Link>
+            <Link href="/results">
+              <a className='hide'></a>
+            </Link>
+            <Link href="/specific/:param">
+              <a className='hide'></a>
+            </Link>
+          </nav>
+          <div onClick={hamburger} id="hamburger" className={open ? ('open') : ('')}>
+            <div className="burger__layer burger__top"></div>
+            <div className="burger__layer burger__middle1"></div>
+            <div className="burger__layer burger__middle2"></div>
+            <div className="burger__layer burger__bottom"></div>
+          </div>
+        </header>
+        <div className='page-container'>{children}</div>
+      </div>
       <Footer />
     </div>
   )
