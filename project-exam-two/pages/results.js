7c3cde2604e195121caded482ce3b451e439d29b
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-09-26 10:29:04 +0200

    Update results.js

--- project-exam-two/pages/results.js
+++ project-exam-two/pages/results.js
@@ -3,19 +3,36 @@ import Head from '../components/Head';
 import AccommCard from '../components/AccommCard';
 
 export default function Results() {
-  let resultsList;
+  let resultsLength = 0;
+  let resultsList = [];
   let input;
   if(typeof window !== 'undefined'){
     resultsList = JSON.parse(localStorage.getItem('ResultsList'));
     input = JSON.parse(localStorage.getItem('Input'));
+    resultsLength = resultsList.length;
   }
+
+  if(!input){
+    return(
+      <Layout pageId='results'>
+        <Head title='Results' description="Find your perfect getaway on Norway's west coast"/>
+        <div className='results__container main'>
+          <div className='results__intro page-intro'>
+            <h1>Results</h1>
+            <p>No results</p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+
   return(
     <Layout pageId='results'>
       <Head title='Results' description="Find your perfect getaway on Norway's west coast"/>
       <div className='results__container main'>
         <div className='results__intro page-intro'>
           <h1>Results</h1>
-          <p>Showing {resultsList.length} result(s) for &quot;{input}&quot;</p>
+          <p>Showing {resultsLength} result(s) for &quot;{input}&quot;</p>
         </div>
         {resultsList.map((item)=>{
           return <AccommCard key={item.id} acc={item} />
